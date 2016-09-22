0688086f1b3e550aa9a7b90af5cba53a6820f7e8
Kishore Routhu <kishore.routhu@wavemaker.com>
2016-09-22 06:40:15 +0000

    New files added - [demo-options.json,EmployeeServiceImpl.java,servicedef.xml,HrdbQueryExecutorService.java,User.java,Department.java,hrdb_procedure.json,servicedef.xml,DepartmentService.java,DepartmentServiceImpl.java,hrdb.properties,DepartmentController.java,hrdb.log,UserService.java,Employee.java,hrdb_published_dataModel.json,package-info.java,UserServiceImpl.java,general-options.json,hrdb-queries.hbm.xml,VacationController.java,hrdb.spring.xml,hrdb_query.json,VacationDao.java,HrdbProcedureExecutorService.java,UserController.java,EmployeeDao.java,hrdb.script,QueryExecutionController.java,hrdb-procedures.mappings.json,DepartmentDao.java,roles.json,ProcedureExecutionController.java,securityService.spring.xml,db-connection-settings.json,VacationServiceImpl.java,EmployeeService.java,intercept-urls.json,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,HrdbProcedureExecutorServiceImpl.java,EmployeeController.java,UserDao.java,Vacation.java] Files modified - [Main.variables.json,Main.html,production.properties,Main.js,project-services.xml,project-security-provider.xml,pom.xml,app.variables.json,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,17 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("liveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
